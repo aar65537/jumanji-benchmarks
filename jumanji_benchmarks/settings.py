0eca5a1c2a9662f5d1250ec1e3cc9b032a51e080
Avi Revah <avraham.a.revah@gmail.com>
2023-06-12 22:22:48 -0500

    bench: vmap over can_move

--- jumanji_benchmarks/settings.py
+++ jumanji_benchmarks/settings.py
@@ -3,9 +3,9 @@ COMMITS = {
     "2e9f0186": "1: switch inside move",
     "ca2e4ba5": "2: no mutate in cond",
     "8f9a67bd": "3: vmap over move_left_row",
-    "e431a5e9": "4: use fori_loop",
-    "77fccd6e": "5: single pass move",
-    "88a51285": "6: implement can move",
+    "77fccd6e": "4: single pass move",
+    "88a51285": "5: implement can move",
+    "f5c34b3c": "6: vmap over can_move",
 }
 
 OPTIONS = {"log_x": True, "markers": True}
